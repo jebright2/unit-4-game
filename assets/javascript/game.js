f55ed0a2f80eefb54f343497ec6dc49ac10ffd00
Da Vinci <jeb2je@gmail.com>
2019-09-02 19:11:28 -0400

    working on randomization function in js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,45 @@
-var crys1 = ;
-var crys2 = ;
-var crys3 = ;
-var crys4 = ;
+var crys1 = [0];
+var crys2 = [1];
+var crys3 = [2];
+var crys4 = [3];
+var min =
+var max = 
 
-var targetnumber =
+var targetNumber = (Math.floor(Math.random() * (max - min + 1)) + min);
 
-var 
+$("#number-to-guess").text(targetNumber);
 
-console.log (Math.floor(Math.random() * (max - min + 1)) + min)
+var counter = 0;
+
+var numberOptions = [10, 5, 3, 7];
+
+ for (var i = 0; i < numberOptions.length; i++) {
+
+  var imageCrystal = $("<img>");
+
+  imageCrystal.addClass("crystal-image");
+
+  imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+
+  imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+  $("#crystals").append(imageCrystal);
+}
+
+ $(".crystal-image").on("click", function() {
+
+  var crystalValue = ($(this).attr("data-crystalvalue"));
+  crystalValue = parseInt(crystalValue);
+
+  counter += crystalValue;
+  alert("New score: " + counter);
+
+  if (counter === targetNumber) {
+    alert("You win!");
+  }
+
+  else if (counter >= targetNumber) {
+    alert("You lose!!");
+  }
+
+});
