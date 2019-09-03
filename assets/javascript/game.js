daca59d794f3c180c613bc2464ac7bb2a90fc498
Da Vinci <jeb2je@gmail.com>
2019-09-03 06:43:48 -0400

    Added randomization to crystals and target on version 2 of code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ var max = 120;
 var win;
 var loss;
 
-var targetNumber = (Math.floor(Math.random() * 121)); 
+var targetNumber = (Math.floor(Math.random() * 102)+19); 
 
  $("#number-to-guess").text(targetNumber);
 
@@ -20,8 +20,6 @@ var numberOptions = [10, 5, 3, 7];
   var imageCrystal = $("<img>");
 
   imageCrystal.addClass("crystal-image");
-
-  imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
 
   imageCrystal.attr("data-crystalvalue", numberOptions[i]);
 
--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -2,15 +2,18 @@ var targetNumber;
 var win;
 var loss;
 
+
+var targetNumber = (Math.floor(Math.random() * 102) + 19); 
+$("#targetNumber").html('Target Number: ' + targetNumber);
 // Add my number numberOptions.lenth back into the code once I know it works
 for (var i = 0; i < 4; i++) {
     
-    var crystalNumber = Math.floor(Math.random() * 12);
+    var crystalNumber = (Math.floor(Math.random() * 12) + 1);
     
     var crystal = $("<div>");
         crystal.attr({
             "class": 'crystal',
-            "data-crystalvalue": random
+            "data-crystalvalue": crystalNumber
         });
 
     $(".crystals").append(crystal);
